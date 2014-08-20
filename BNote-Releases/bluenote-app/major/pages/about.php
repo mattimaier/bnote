17888eeeadc2253648ddf3b5bc61368d15cc70bf
Matti Maier <mattimaieris@gmail.com>
2014-08-20 11:57:27 +0200

    Cleanup release folder; Packaged patch 2.5.2

--- BNote-Releases/bluenote-app/major/pages/about.php
+++
@@ -1,7 +0,0 @@
-<h3>Kontaktinformationen des Entwicklers</h3>
-<p><b>Matti Maier Internet Solutions</b><br />
-Parkstr. 18 &#183; 73630 Remshalden<br />
-<a href="http://www.mattimaier.de" target="_blank" data-role="button">Website</a><br />
-<a href="mailto:info@mattimaier.de" data-role="button">E-Mail senden</a><br />
-<a href="tel:+498980912915" data-role="button">Anrufen</a>
-</p>
--- BNote-Releases/bluenote-app/major/pages/reason.php
+++
@@ -1,6 +0,0 @@
-<form id="reason_form" method="POST">
-	<label for="reason">Bitte gebe eine kurze Begründung an</label>
-	<input type="text" name="reason" />
-	<input type="hidden" name="rid" value="0" />
-	<button type="submit" data-theme="a" name="submit" value="submit-value">Nicht Teilnehmen</button>
-</form>
