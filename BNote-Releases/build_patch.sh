3c06c7a5f25f3741c7f8bb4f426c0a7cbf48195e
Matti Maier <info@mattimaier.de>
2016-09-11 15:13:09 +0200

    BNote Patch v3.1.3

---
+++ BNote-Releases/build_patch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+tmp_dir="BNote-Releases/tmp"
+
+# Check if the first argument is set
+if [ -z $1 ]; then
+ echo 'usage: build_patch.sh release-commit'
+fi
+
+# Change to root dir of BNote Repo
+cd ..
+
+# Check if the tmp folder exists -> if so, clean it otherwise create it
+if [ -d "$tmp_dir" ]; then
+ rm -r $tmp_dir/*
+else
+ mkdir $tmp_dir
+fi
+
+# Copy changed files
+git diff -z --name-only HEAD $1 | xargs -0 -IREPLACE rsync -aR REPLACE $tmp_dir
+
+# Clean Mac stuff
+find $tmp_dir -name .DS_Store -delete
+
+# Create zip file
+zip -r BNote-Releases/BNote/Patches/bnote_latest_patch.zip $tmp_dir
