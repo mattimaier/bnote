2ed6e15d41186400958d4e7bdc312613fd4c5926
Matti Maier <info@mattimaier.de>
2018-12-29 13:22:40 +0100

    #332: semi-fix for leaving webapp context in ios

--- BNote-App/bnote/share.controller.js
+++ BNote-App/bnote/share.controller.js
@@ -3,11 +3,32 @@ sap.ui.controller("bnote.share", {
 	onAfterRendering: function() {
     	var oCtrl = this;
         
+        var dest = "../BNote/embed.php?mod=12&mobilePin=" + mobilePin;
         var model = new sap.ui.model.json.JSONModel({
-            "framecode": "<iframe style='width:100%;height:98%;z-index:-1;overflow-y: hidden;' width='100%' height='100%' "
-                     + "src='../BNote/embed.php?mod=12&mobilePin=" + mobilePin + "'></iframe>"
+            "framecode": "<iframe id='shareframe' style='width:100%;height:98%;z-index:-1;overflow-y: hidden;' width='100%' height='100%' "
+                     + "src='" + dest + "'></iframe>"
         });
         oCtrl.getView().setModel(model);
+        oCtrl.loadIframe(dest);
+    },
+    
+    stayInWebapp: function() {
+        var oCtrl = this;
+        if(("standalone" in window.navigator) && window.navigator.standalone) {
+            jQuery('#shareframe').contents().find('a').click(function(event) {
+                var dest = jQuery(this).attr("href");
+                event.preventDefault();
+                oCtrl.loadIframe(dest);
+                });
+        }
+        return true;
+    },
+    
+    loadIframe: function(dest) {
+        var oCtrl = this;
+        var node = jQuery('#shareframe');
+        node.attr("src", dest);        
+        oCtrl.stayInWebapp();
     }
 
 });
