214721f0eb9f76ae4c51c2bdef6d1b417533808e
encodeuramateur <51021997+encodeuramateur@users.noreply.github.com>
2022-07-27 20:06:28 +0200

    Update regex.php

--- BNote/src/data/regex.php
+++ BNote/src/data/regex.php
@@ -4,7 +4,7 @@
 **/
 class Regex {
 
-	public static $SPECIALCHARACTERS = "àáâãăäāåæćčçèéêĕëēìíîĭïðłñòóôõöőøœšùúûüűýÿþÀÁÂÃĂÄĀÅÆĆČÇÈÉÊĔËĒÌÍÎĬÏÐŁÑÒÓÔÕÖŐØŒŠÙÚÛÜŰÝÞß";
+	public static $SPECIALCHARACTERS = "'`,-àáâãăäāåæćčçèéêĕëēìíîĭïðłñòóôõöőøœšùúûüűýÿþÀÁÂÃĂÄĀÅÆĆČÇÈÉÊĔËĒÌÍÎĬÏÐŁÑÒÓÔÕÖŐØŒŠÙÚÛÜŰÝÞß";
 	
 	private $regex;
 	private $regex_js;
